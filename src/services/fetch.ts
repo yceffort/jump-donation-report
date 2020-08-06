dfb63b4889f140de7f26e5aea1aaa9177dacb1c4
강주히 <vkdlfl0224@naver.com>
2020-08-06 15:49:54 +0900

    [#12] result 할당 제거

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -4,6 +4,5 @@ import { GoogleSignInInterface } from './interfaces';
 export default async function getUser(token: string): Promise<GoogleSignInInterface> {
   const url = `https://us-central1-jumpsp-admin.cloudfunctions.net/jump-google-sign-in?token=${token}`;
   const response = await fetch(url);
-  const result = await response.json();
-  return result;
+  return response.json();
 }
