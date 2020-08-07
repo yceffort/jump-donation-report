c695af3bc41343875743e3fa013b17c80c76043a
yceffort <yceffort@gmail.com>
2020-08-07 17:34:56 +0900

    [#12] 새로고침 이후에도 로그인이 유지될 수 있도록 변경

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,49 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
-import getUser from '../services/fetch';
 import { userReducer, initialState, UserStatus } from '../common/user/reducer';
+import { GoogleUserInfoInterface } from '../common/interfaces';
+import getUser from '../services/fetch';
 
 const LOGIN_COOKIE = 'LOGIN_SESSION';
 const CLIENT_ID = '450893975309-gr4ed18b4733vapf59l35e150i927vlq.apps.googleusercontent.com';
-export default function Login() {
+
+export default function Login({
+  token,
+  userInfo,
+}: {
+  token?: string
+  userInfo?: GoogleUserInfoInterface
+}) {
   const [userState, dispatch] = useReducer(userReducer, initialState);
 
   const [, setCookie, removeCookie] = useCookies([LOGIN_COOKIE]);
+
+  useEffect(() => {
+    if (userInfo && token) {
+      dispatch({
+        status: UserStatus.Login,
+        user: {
+          email: userInfo.email,
+          exp: userInfo.exp,
+          name: userInfo.name,
+          token,
+        },
+      });
+    }
+  }, [userInfo, token]);
 
   async function signOut() {
     dispatch({
       status: UserStatus.LogOut,
     });
     removeCookie(LOGIN_COOKIE);
-    await window.gapi.auth2.getAuthInstance().signOut();
+    window.gapi.load('auth2', async () => {
+      window.gapi.auth2.init({
+        client_id: CLIENT_ID,
+      });
+      const GoogleAuth = window.gapi.auth2.getAuthInstance();
+      await GoogleAuth.signOut();
+    });
   }
 
   const onSignIn = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -33,14 +61,11 @@ export default function Login() {
 
       const response = await GoogleAuth.signIn(options);
       const authResponse = response.getAuthResponse();
-      const token = authResponse.id_token;
-      const { result, info } = await getUser(token);
+      const { result, info } = await getUser(authResponse.id_token);
 
       if (result && info) {
-        const expDate = info.exp
-          ? new Date(info.exp)
-          : new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-        setCookie(LOGIN_COOKIE, token, { path: '/', expires: expDate });
+        const expDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+        setCookie(LOGIN_COOKIE, authResponse.id_token, { path: '/', expires: expDate });
 
         dispatch({
           status: UserStatus.Login,
@@ -48,6 +73,7 @@ export default function Login() {
             email: info.email,
             exp: info.exp,
             name: info.name,
+            token: authResponse.id_token,
           },
         });
       } else {
